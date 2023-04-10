799122640bb11bd6d1d4e05bced2c1314cbd1180
jakobob27 <jakob.huuse@gmail.com>
2023-04-10 16:50:46 +0200

    Fixed a minor annoyance

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.jackern {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires transitive javafx.graphics;
 
     opens com.jackern to javafx.fxml;
 
